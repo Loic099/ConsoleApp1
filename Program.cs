00359dff1a63c07a7fb4e75b7f677a5d6ff25821
Loic099 <lo.simonis@student.helmo.be>
2026-02-02 15:53:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
+
+
+double rayon = 2.5;
+double aire = Math.PI * rayon * rayon;
+
+Console.WriteLine(aire);
+
+Fonction();
+
+void Fonction()
+{
+    int a = 5;
+    int b = 5;
+    if (a + b > 10)
+        Console.WriteLine("The answer is greater than 10");
+    else
+        Console.WriteLine("The answer is not greater than 10");
+
+}
